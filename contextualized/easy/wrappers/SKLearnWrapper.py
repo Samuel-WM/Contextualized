5f0ba2c63795c4d5ec7729bac0728428589bab14
Peter Li <peterli@Peters-MacBook-Pro-2.local>
2025-07-24 16:23:03 -0500

    Expand encoder kwargs to width layer link_fn thoroughly in SKlearn

--- contextualized/easy/wrappers/SKLearnWrapper.py
+++ contextualized/easy/wrappers/SKLearnWrapper.py
@@ -99,7 +99,9 @@ class SKLearnWrapper:
                 "link_fn",
                 "univariate",
                 "encoder_type",
-                "encoder_kwargs",
+                "encoder_width",
+                "encoder_layers",
+                "encoder_link_fn",
                 "model_regularizer",
                 "num_archetypes",
                 "learning_rate",
@@ -143,16 +145,16 @@ class SKLearnWrapper:
             "encoder_link_fn",
         ]
         self.constructor_kwargs = self._organize_constructor_kwargs(**kwargs)
-        self.constructor_kwargs["encoder_kwargs"]["width"] = kwargs.pop(
-            "width", self.constructor_kwargs["encoder_kwargs"]["width"]
+        self.constructor_kwargs["encoder_width"] = kwargs.pop(
+            "width", self.constructor_kwargs["encoder_width"]
         )
-        self.constructor_kwargs["encoder_kwargs"]["layers"] = kwargs.pop(
-            "layers", self.constructor_kwargs["encoder_kwargs"]["layers"]
+        self.constructor_kwargs["encoder_layers"] = kwargs.pop(
+            "layers", self.constructor_kwargs["encoder_layers"]
         )
-        self.constructor_kwargs["encoder_kwargs"]["link_fn"] = kwargs.pop(
+        self.constructor_kwargs["encoder_link_fn"] = kwargs.pop(
             "encoder_link_fn",
-            self.constructor_kwargs["encoder_kwargs"].get(
-                "link_fn", self.default_encoder_link_fn
+            self.constructor_kwargs.get(
+                "encoder_link_fn", self.default_encoder_link_fn
             ),
         )
         self.not_constructor_kwargs = {
@@ -307,16 +309,11 @@ class SKLearnWrapper:
         maybe_add_constructor_kwarg("univariate", False)
         maybe_add_constructor_kwarg("encoder_type", self.default_encoder_type)
         maybe_add_constructor_kwarg("loss_fn", LOSSES["mse"])
-        maybe_add_constructor_kwarg(
-            "encoder_kwargs",
-            {
-                "width": kwargs.get("encoder_width", self.default_encoder_width),
-                "layers": kwargs.get("encoder_layers", self.default_encoder_layers),
-                "link_fn": kwargs.get("encoder_link_fn", self.default_encoder_link_fn),
-            },
-        )
+        maybe_add_constructor_kwarg("encoder_width", self.default_encoder_width)
+        maybe_add_constructor_kwarg("encoder_layers", self.default_encoder_layers)
+        maybe_add_constructor_kwarg("encoder_link_fn", self.default_encoder_link_fn)
         if kwargs.get("subtype_probabilities", False):
-            constructor_kwargs["encoder_kwargs"]["link_fn"] = LINK_FUNCTIONS["softmax"]
+            constructor_kwargs["encoder_link_fn"] = LINK_FUNCTIONS["softmax"]
 
         # Make regularizer
         if "model_regularizer" in self.acceptable_kwargs["model"]:
